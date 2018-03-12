96355fe552516ccf1bb91952be93a83bfbf2ba28
Andrea Crawford <andrea.crawford13@gmail.com>
2018-03-13 00:19:34 +0100

    Filter recipes by search term

--- src/components/RecipeBrowser/RecipeBrowser.js
+++ src/components/RecipeBrowser/RecipeBrowser.js
@@ -4,11 +4,19 @@ import RecipeList from './RecipeList/RecipeList';
 import SearchBar from '../Navigation/SearchBar/SearchBar';
 
 const recipeBrowser = (props) => {
+    const filteredRecipes = props.recipes
+        .filter(recipe => {
+            return recipe.ingredients
+                .filter(ingredient => {
+                    return ingredient.name.match(new RegExp(props.searchTerm, 'i'))
+                }).length > 0
+            });
+
     return (
         <div>
             <SearchBar changed={props.onSearchTermUpdated} searchTerm={props.searchTerm} />
             <div className="main-container">
-                <RecipeList recipes={props.recipes} />
+                <RecipeList recipes={filteredRecipes} />
             </div>
         </div>
     )

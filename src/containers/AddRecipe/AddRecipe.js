034de140b40555636e3d2211c06fe9dd47272927
Andrea Crawford <andrea.crawford13@gmail.com>
2018-03-13 18:01:44 +0100

    Remove unused methods

--- src/containers/AddRecipe/AddRecipe.js
+++ src/containers/AddRecipe/AddRecipe.js
@@ -25,14 +25,17 @@ class addRecipe extends Component {
                         amount: {
                             type: 'text',
                             placeholder: 'Amount',
+                            value: '',
                         },
                         measurement: {
                             type: 'text',
                             placeholder: 'Measurement',
+                            value: '',
                         },
                         name: {
                             type: 'text',
                             placeholder: 'Ingredient Name',
+                            value: '',
                         },
                     },
                     fields: {},
@@ -63,140 +66,7 @@ class addRecipe extends Component {
                 }
             }
         })
-    }
-
-    addFieldHandler = (e, type) => {
-        e.preventDefault();
-        this.setState({
-            fields: {
-                ...this.state.fields,
-                name2: {
-                    type: 'text',
-                    placeholder: 'dd name',
-                    value: '',
-                }
-            }
-        })
-    }
-    
-    newSeasoningHandler = (e) => {
-        console.log('new seasoning handler');
-        this.setState({
-            form: {
-                ...this.state.form,
-                sections: {
-                    ...this.state.form.sections,
-                    seasonings: {
-                        ...this.state.form.sections.seasonings,
-                        activeField: {
-                            ...this.state.form.sections.seasonings.activeField,
-                            value: e.target.value,
-                        },
-                    }
-                }
-            }
-        })
-    }
-
-    removeSeasoningHandler = (e, id) =>{
-        e.preventDefault();
-        const section = this.state.form.sections.seasonings;
-        const updatedFields = section.fields;
-        delete updatedFields[id];
-        console.log(updatedFields);
-
-        this.setState({
-            form: {
-                ...this.state.form,
-                sections: {
-                    ...this.state.form.sections,
-                    seasonings: {
-                        ...this.state.form.sections.seasonings,
-                        fields: updatedFields,
-                    }
-                }
-            }
-        });
-    }
-    
-    updateSeasoningHandler = (e, id) => {
-        const section = this.state.form.sections.seasonings;
-        const editingField = section.fields[id];
-        const updatedField = {
-            ...editingField,
-            value: e.target.value,
-        }
-        this.setState({
-            form: {
-                ...this.state.form,
-                sections: {
-                    ...this.state.form.sections,
-                    seasonings: {
-                        ...section,
-                        fields: {
-                            ...section.fields,
-                            [id]: updatedField,
-                        }
-                    }
-                }
-            }
-        })
-    }
-
-    addSeasoningHandler = (e) => {
-        console.log('add seasoning handler');
-        e.preventDefault();
-        const currentFields = {...this.state.form.sections.seasonings.fields};
-        currentFields[this.state.form.sections.seasonings.activeField.id] = {value: this.state.form.sections.seasonings.activeField.value};
-        this.setState({
-            form: {
-                ...this.state.form,
-                sections: {
-                    ...this.state.form.sections,
-                    seasonings: {
-                        ...this.state.form.sections.seasonings,
-                        fields: currentFields,
-                        activeField: {
-                            value: '',
-                            id: 'seasonings' + new Date().getTime(),
-                        }
-                    }
-                }
-            }
-        })
-    }
-
-    addInputHandlers = (e, sectionId) => {
-        e.preventDefault();
-        // store reference to section
-        const section = this.state.form.sections[sectionId];
-        // copy this section's fields
-        const currentFields = {...section.fields};
-        // move active field to fields array.
-        currentFields[sectionId + new Date().getTime()] = {...section.defaultFields};
-        const defaultFields = {...section.defaultFields};
-        console.log(defaultFields);
-        for (let key in defaultFields) {
-            const defaultField = {...defaultFields[key]};
-            defaultField.value = '';
-            defaultFields[key] = defaultField;
-        }
-        console.log(defaultFields);
-        this.setState({
-            form: {
-                ...this.state.form,
-                sections: {
-                    ...this.state.form.sections,
-                    [sectionId]: {
-                        ...section,
-                        fields: currentFields,
-                        defaultFields: defaultFields,
-                    }
-                }
-            }
-        })
-    }
-    
+    }    
 
     addInputHandler = (e, sectionId) => {
         e.preventDefault();
@@ -301,7 +171,6 @@ class addRecipe extends Component {
                         key={field.id}
                         value={field.value}
                         placeholder={field.placeholder} />))}
-                    <button onClick={(e) => {this.addFieldHandler(e, 'text')}}>add field</button>
                     <IngredientsFormSection
                         onChangeHandler={this.editActiveInputHandler}
                         onEditHandler={this.editExistingInputHandler}

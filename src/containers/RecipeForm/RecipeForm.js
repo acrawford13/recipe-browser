a46b0df7e5eae3c3b615fed3e305a2cd2717282a
Andrea Crawford <andrea.crawford13@gmail.com>
2018-03-15 20:17:46 +0100

    Format data for post request

--- src/containers/RecipeForm/RecipeForm.js
+++ src/containers/RecipeForm/RecipeForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import update from 'immutability-helper';
+import axios from 'axios';
 
 import MultiFieldFormSection from '../../components/UI/Forms/Sections/MultiFieldFormSection/MultiFieldFormSection';
 import Input from '../../components/UI/Forms/Input/Input';
@@ -215,11 +216,85 @@ class recipeForm extends Component {
                 </div>)
             }
         });
+        
+        const dataToArray = (data) => {
+            return Object.keys(data).map(
+                dataId => Object.keys(data[dataId]).map(
+                    fieldId => data[dataId][fieldId].value
+                )
+            );
+        }
+        
+        const dataToObject = (data) => {
+            return Object.keys(data).map(
+                dataId => {
+                    const value = {};
+                    for (let key in data[dataId]){
+                        value[key] = data[dataId][key].value;
+                    }
+                    return value;
+                }
+            )
+        }
 
         return (
             <form className="form" onSubmit={(e) => {
                     e.preventDefault();
-                    console.log(formData);
+                    let submitData = {...this.state.form.fields};
+
+                    for (let fieldName in submitData) {
+                        let field = {...submitData[fieldName]};
+                        if (field.type === 'multi') {
+                            let formFields = Object.keys(field.defaultFields);
+                            if(formFields.length === 1){
+                                // if there is only one default field, submit data as an array
+                                field = dataToArray(field.data);
+                            } else {
+                                // else submit data as an object with field names
+                                field = dataToObject(field.data);
+                            }
+                        } else if (field.type === 'file') {
+                            if(field.data){
+                                field = field.data;
+                            }
+                        } else {
+                            field = field.value
+                        }
+                    }
+
+                    // let submitData = {};
+                    // for (let key in this.state.form.fields) {
+                    //     console.log(this.state.form.fields[key]);
+                    //     if(!this.state.form.fields[key].hasOwnProperty('data')){
+                    //         // add data from normal fields
+                    //         submitData[key] = {value: this.state.form.fields[key].value}
+                    //     } else {
+                    //         let subData = [];
+                    //         if (Object.keys(this.state.form.fields[key].data).length===1) {
+                    //             console.log(Object.keys(this.state.form.fields[key].data));
+                    //         } else {
+                                
+                    //         for (let key2 in this.state.form.fields[key].data) {
+                    //             let subSubData = {};
+                    //             if(Object.keys(this.state.form.fields[key].data[key2]).length === 1){
+                    //                 subSubData[key2] = this.state.form.fields[key].data[key2].value;
+                    //             } else {
+                    //                 for (let key3 in this.state.form.fields[key].data[key2]) {
+                    //                     if (Object.keys(this.state.form.fields[key].data[key2]).length === 1) {
+                    //                         subSubData = this.state.form.fields[key].data[key2][key3].value;
+                    //                     } else {
+                    //                         // put object keys into array
+                    //                         subSubData[key3] = this.state.form.fields[key].data[key2][key3].value
+                    //                     }
+                    //                 }
+                    //             }
+                    //             subData.push(subSubData);
+                    //         }
+                    //         submitData[key] = subData;
+                    //         }
+                    //     }
+                    // }
+                    console.log(submitData);
                 }}>
                 {formData}
                 <input type="submit" value="Submit" />

a9be73f02bcf25e399914120c207d98c65d2a4f8
Andrea Crawford <andrea.crawford13@gmail.com>
2018-03-16 16:45:41 +0100

    Add spinner to recipe view

--- src/components/Pages/RecipeView/RecipeView.js
+++ src/components/Pages/RecipeView/RecipeView.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 import NavigationBar from '../../Navigation/NavigationBar/NavigationBar';
+import Spinner from '../../UI/Spinner/Spinner';
 
 const formatIngredient = (ingredient) => {
     if (['g','kg'].indexOf(ingredient.unit) === -1){
@@ -9,34 +11,56 @@ const formatIngredient = (ingredient) => {
     return ingredient.amount + ingredient.unit + " " + ingredient.name;
 }
 
+
+
 const recipeView = (props) => {
-    return (
-        <div>
-            <NavigationBar toHome addRecipe />
-            <div className="main-container">
+    let recipe = <Spinner />
+    if (!props.loading) {
+        if(props.recipe) {
+            const recipeIngredients = (
+                <div>
+                    <h4 className="recipe-detail__subtitle">Ingredients</h4>
+                    <ul>{props.recipe.ingredients.map((ingredient, index) => <li key={index}>{formatIngredient(ingredient)}</li>)}</ul>
+                </div>
+            )
+
+            const recipeSeasonings = (
+                <div>
+                    <h4 className="recipe-detail__subtitle">Seasonings</h4>
+                    <ul>{props.recipe.seasonings.map((seasoning, index) => <li key={index}>{seasoning}</li>)}</ul>
+                </div>
+            )
+
+            const recipeSteps = (
+                <div>
+                        <h4 className="recipe-detail__subtitle">Steps</h4>
+                        <ol>
+                            {props.recipe.steps.map((step, index) => <li key={index}>{step}</li>)}
+                        </ol>
+                </div>
+            )
+
+            recipe = (
                 <div className="recipe-detail">
                     <h2 className="recipe-detail__title">{props.recipe.name}</h2>
                     <div className="recipe-detail__content">
                         <p className="recipe-detail__description">{props.recipe.description}</p>
                         <img className="recipe-detail__image" src={props.recipe.image} alt={props.recipe.name + ' photo'} />
-                        <div className="recipe-detail__ingredients">
-                            <h4 className="recipe-detail__subtitle">Ingredients</h4>
-                            <ul>
-                                {props.recipe.ingredients.map((ingredient, index) => <li key={index}>{formatIngredient(ingredient)}</li>)}
-                            </ul>
-                            <h4 className="recipe-detail__subtitle">Seasonings</h4>
-                            <ul>
-                                {props.recipe.seasonings.map((seasoning, index) => <li key={index}>{seasoning}</li>)}
-                            </ul>
-                        </div>
-                        <div className="recipe-detail__method">
-                            <h4 className="recipe-detail__subtitle">Steps</h4>
-                            <ol>
-                                {props.recipe.steps.map((step, index) => <li key={index}>{step}</li>)}
-                            </ol>
-                        </div>
+                        {props.recipe.ingredients ? recipeIngredients : ''}
+                        {props.recipe.seasonings ? recipeSeasonings : ''}
+                        {props.recipe.steps ? recipeSteps : ''}                
                     </div>
                 </div>
+            );
+        } else {
+            recipe = <Redirect to="/" />;
+        }
+    };
+    return (
+        <div>
+            <NavigationBar toHome addRecipe />
+            <div className="main-container">
+                {recipe}
             </div>
         </div>
     );

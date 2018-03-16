aafd9e8baca26f8f4797ebc22dec05ef37970a51
Andrea Crawford <andrea.crawford13@gmail.com>
2018-03-16 13:53:59 +0100

    Change spinner to GIF

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import spinnerImg from '../../../assets/spinner.gif';
+
 const spinner = () => {
-    return <div className="spinner">Loading...</div>
+    return <div className="spinner"><img height="100" src={spinnerImg} alt="Loading..." /></div>
 }
  
 export default spinner;

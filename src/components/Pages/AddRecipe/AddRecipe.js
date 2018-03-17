4b32ad0c7b4e7f5439dcbb257dd38ed9ead4603f
Andrea Crawford <andrea.crawford13@gmail.com>
2018-03-17 01:17:57 +0100

    Move addRecipe file to pages

---
+++ src/components/Pages/AddRecipe/AddRecipe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RecipeForm from '../../../containers/RecipeForm/RecipeForm';
+import NavigationBar from '../../Navigation/NavigationBar/NavigationBar';
+
+const addRecipe = (props) => {
+    return (
+        <div>
+        <NavigationBar toHome />
+        <div className="main-container">
+            <RecipeForm />
+        </div>
+        </div>
+    )
+}
+ 
+export default addRecipe;
